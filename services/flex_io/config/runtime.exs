c334b6addb64ddda7ac3946c26444e392086e9d3
Light Play <lightplay456@gmail.com>
2026-07-01 19:36:52 +0300

    fix: исправлены ошибки после запуска и при сборке

--- services/flex_io/config/runtime.exs
+++ services/flex_io/config/runtime.exs
@@ -40,8 +40,7 @@ if config_env() == :prod do
     url: [host: host, port: 443, scheme: "https"],
     http: [
       ip: {0, 0, 0, 0},
-      port: port,
-      parser_options: [json: [max_length: 100_000_000]]
+      port: port
     ],
     secret_key_base: secret_key_base,
     server: true,
